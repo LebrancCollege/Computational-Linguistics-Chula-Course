decfd8989e1d81828cf5f7b8afd618c1bbe22397
lebrancconvas <lebrancconvas@gmail.com>
2026-02-06 20:03:29 +0700

    ✨feat: update assignment codes.

--- assignment/assignment_01_text_classification/logistic_regression.py
+++ assignment/assignment_01_text_classification/logistic_regression.py
@@ -6,7 +6,7 @@ import pythainlp
 class TextClassifier:
 
     def __init__(self, csv_file_name):
-        self.model_params = pd.read_csv(csv_file_name, index_col=0)
+        self.model_params = pd.read_csv(csv_file_name, index_col=0) 
 
     def compute_probability(self, text_string):
         pass
@@ -16,7 +16,7 @@ class TextClassifier:
         # pass
 
     def get_all_possible_labels(self):
-        return self.model_params["word"]
+        return self.model_params.index   
         pass
 
     def classify(self, text_string):
@@ -26,12 +26,14 @@ class TextClassifier:
 if __name__ == '__main__':
     if (len(sys.argv) != 2):
         data = TextClassifier("toy_model.csv")
+        # data = TextClassifier("toy_model2.csv")
         print('usage:\tpython logistic_regression.py <model_file>')
         print(data.model_params)
         print(data.get_all_possible_features())
-        
-
         print(data.get_all_possible_labels())
+        text = "Hi Bro, I love you"
+        print(data.compute_probability(text))
+        print(data.classify(text))  
         sys.exit(0)
     model_file_name = sys.argv[1]
     model = TextClassifier(model_file_name)
